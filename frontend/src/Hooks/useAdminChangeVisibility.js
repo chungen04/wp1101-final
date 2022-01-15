12c70e1cf0bee24216745f435ca953d505c810ed
chungen04 <root@DESKTOP-2FOBT56.localdomain>
2022-01-15 23:34:17 +0800

    update uis

--- frontend/src/Hooks/useAdminChangeVisibility.js
+++ frontend/src/Hooks/useAdminChangeVisibility.js
@@ -1,214 +1,233 @@
-import {useState} from "react";
+import { useState } from "react";
 import React from "react";
 
 import {
-    CardContent,
-    Grid,
-    List,
-    ListItem, 
-    ListItemText,
-    Divider,
-    Card
-} from '@mui/material';
+  CardContent,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  Card,
+} from "@mui/material";
 
-import {
-    Button,
-    Typography
-} from '@material-ui/core';
+import { Button, Typography } from "@material-ui/core";
 
-import DeleteIcon from '@mui/icons-material/Delete';
-import RefreshIcon from '@mui/icons-material/Refresh';
+import DeleteIcon from "@mui/icons-material/Delete";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
-const useAdminChangeVisibility = () =>{
-    const [documentType, setDocumentType] = useState("")
-    const [courseFilter, setCourseFilter] = useState({})
-    const [examFilter, setExamFilter] = useState({})
-    const [fileFilter, setFileFilter] = useState({})
-    const [queryData, setQueryData] = useState([])
-    const [query, setQuery] = useState({});
+const useAdminChangeVisibility = () => {
+  const [documentType, setDocumentType] = useState("");
+  const [courseFilter, setCourseFilter] = useState({});
+  const [examFilter, setExamFilter] = useState({});
+  const [fileFilter, setFileFilter] = useState({});
+  const [queryData, setQueryData] = useState([]);
+  const [query, setQuery] = useState({});
 
-
-    const ShowMore = (content) => {
-      return(
-        <List disablePadding>
-            <Typography variant="h6" gutterBottom>
-            Details
-            </Typography>
-            {Object.keys(content).map((key) => (
-            <ListItem key={key}>
-                <ListItemText primary={key} sx={{ padding: 1 }}/>
-                <Grid item >
-                <Typography variant="body2">{content[key]}</Typography>
-                </Grid>
-            </ListItem>
-            ))}
-        </List>
-      )
-    }
-    
-      const CardForExam = ({content, handleDelete, handleChangeVisibility}) => {
-        const {
-            year,
-            semester,
-            instructors,
-            department,
-            courseName,
-            examName,
-            examTime,
-            show
-        } = content;
-        return(
-          <Card sx={{my: 2}} style={!show? {backgroundColor: "#00000011"}: {backgroundColor: "#90F51DDD"}}>
-          <CardContent>
-          <Grid container>
-            <Grid item xs>
-            <Typography variant="h5" component="div">
-            {[courseName, department, examName].join(" - ")}
-            </Typography>
-            <Typography variant="h6" component="div">
-            {[year, semester].join(" - ")}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }}>
-              Instructor: {instructors}
-            </Typography>
-            <Typography variant="body2">
-              ExamTime: {examTime} <br></br>
-            </Typography>
-            <Typography variant="body2">
-                Currently Show: {show? "true": "false"}
-            </Typography>
-            <Typography variant="body2">
-                Files contained: {content.files.length}
-            </Typography>
+  const ShowMore = (content) => {
+    return (
+      <List disablePadding>
+        <Typography variant="h6" gutterBottom>
+          Details
+        </Typography>
+        {Object.keys(content).map((key) => (
+          <ListItem key={key}>
+            <ListItemText primary={key} sx={{ padding: 1 }} />
+            <Grid item>
+              <Typography variant="body2">{content[key]}</Typography>
             </Grid>
-          <Divider orientation="vertical" flexItem/>
-            <Grid item xs>
-            <Typography variant="h6" component = "div">  
-              &nbsp;&nbsp;Options
-            </Typography>
-            
-          
-          <Button onClick = {() =>{
-            handleDelete({
-              variables:{
-                examId: content.examId
-              }
-            })
-          }}>
-            <DeleteIcon color = "action"/> Delete this Exam
-          </Button><br></br>
-          <Button onClick = {() =>{
-            handleChangeVisibility({
-              variables:{
-                examId: content.examId,
-                show: !content.show
-              }
-            })
-          }}>
-            <RefreshIcon color = "action"/> Change Visibility
-          </Button><br></br>
-          </Grid>
-          </Grid>
-          </CardContent>
-          </Card>
-        )
-      };
-      const CardForFile = ({content, handleDelete, handleChangeVisibility}) => {
-        const {
-            year,
-            semester,
-            instructors,
-            department,
-            courseName,
-            examName,
-            examTime,
-            questionViewLink,
-            answerViewLink,
-            show,
-            remarks
-        } = content;
-        return(
-          <Card sx={{my: 2}} style={!show? {backgroundColor: "#00000011"}: {backgroundColor: "#90F51DDD"}}>
-          <CardContent>
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+
+  const CardForExam = ({ content, handleDelete, handleChangeVisibility }) => {
+    const {
+      year,
+      semester,
+      instructors,
+      department,
+      courseName,
+      examName,
+      examTime,
+      show,
+    } = content;
+    return (
+      <Card
+        sx={{ my: 2 }}
+        style={
+          show
+            ? { backgroundColor: "#00000011" }
+            : { backgroundColor: "#90F51DDD" }
+        }
+      >
+        <CardContent>
           <Grid container>
             <Grid item xs>
-            <Typography variant="h5" component="div">
-            {[courseName, department, examName].join(" - ")}
-            </Typography>
-            <Typography variant="h6" component="div">
-            {[year, semester].join(" - ")}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }}>
-              Instructor: {instructors}
-            </Typography>
-            <Typography variant="body2">
-              ExamTime: {examTime} <br></br>
-            </Typography>
-            <Typography variant="body2">
-                Currently Show: {show? "true": "false"}
-            </Typography>
-            <Typography variant="body2">
+              <Typography variant="h5" component="div">
+                {[courseName, department, examName].join(" - ")}
+              </Typography>
+              <Typography variant="h6" component="div">
+                {[year, semester].join(" - ")}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }}>
+                Instructor: {instructors}
+              </Typography>
+              <Typography variant="body2">
+                ExamTime: {examTime} <br></br>
+              </Typography>
+              <Typography variant="body2">
+                Currently Show: {show ? "true" : "false"}
+              </Typography>
+              <Typography variant="body2">
+                Files contained: {content.files.length}
+              </Typography>
+            </Grid>
+            <Divider orientation="vertical" flexItem />
+            <Grid item xs>
+              <Typography variant="h6" component="div">
+                &nbsp;&nbsp;Options
+              </Typography>
+
+              <Button
+                onClick={() => {
+                  handleDelete({
+                    variables: {
+                      examId: content.examId,
+                    },
+                  });
+                }}
+              >
+                <DeleteIcon color="action" /> Delete this Exam
+              </Button>
+              <br></br>
+              <Button
+                onClick={() => {
+                  handleChangeVisibility({
+                    variables: {
+                      examId: content.examId,
+                      show: !content.show,
+                    },
+                  });
+                }}
+              >
+                <RefreshIcon color="action" /> Change Visibility
+              </Button>
+              <br></br>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    );
+  };
+  const CardForFile = ({ content, handleDelete, handleChangeVisibility }) => {
+    const {
+      year,
+      semester,
+      instructors,
+      department,
+      courseName,
+      examName,
+      examTime,
+      questionViewLink,
+      answerViewLink,
+      show,
+      remarks,
+    } = content;
+    return (
+      <Card
+        sx={{ my: 2 }}
+        style={
+          !show
+            ? { backgroundColor: "#00000011" }
+            : { backgroundColor: "#90F51DDD" }
+        }
+      >
+        <CardContent>
+          <Grid container>
+            <Grid item xs>
+              <Typography variant="h5" component="div">
+                {[courseName, department, examName].join(" - ")}
+              </Typography>
+              <Typography variant="h6" component="div">
+                {[year, semester].join(" - ")}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }}>
+                Instructor: {instructors}
+              </Typography>
+              <Typography variant="body2">
+                ExamTime: {examTime} <br></br>
+              </Typography>
+              <Typography variant="body2">
+                Currently Show: {show ? "true" : "false"}
+              </Typography>
+              <Typography variant="body2">
                 Question View Link:
-                <a href = {questionViewLink}>Click Me</a>
-            </Typography>
-            <Typography variant="body2">
+                <a href={questionViewLink}>Click Me</a>
+              </Typography>
+              <Typography variant="body2">
                 Answer View Link:
-                {answerViewLink? (
-                  <a href = {answerViewLink}>Click Me</a>
-                ):(
+                {answerViewLink ? (
+                  <a href={answerViewLink}>Click Me</a>
+                ) : (
                   "None"
                 )}
-            </Typography>
-            <Typography variant="body2">
-                Remarks: {remarks}
-            </Typography>
+              </Typography>
+              <Typography variant="body2">Remarks: {remarks}</Typography>
             </Grid>
-            <Divider orientation="vertical" flexItem/>
+            <Divider orientation="vertical" flexItem />
             <Grid item xs>
-            <Typography variant="h6" component = "div">  
-              &nbsp;&nbsp;Options
-            </Typography>
-          <Button onClick = {() =>{
-            handleDelete({
-              variables:{
-                fileId: content.fileId
-              }
-            })
-          }}>
-            <DeleteIcon color = "action"/> Delete this File
-          </Button><br></br>
-          <Button onClick = {() =>{
-            handleChangeVisibility({
-              variables:{
-                fileId: content.fileId,
-                show: !content.show
-              }
-            })
-          }}>
-            <RefreshIcon color = "action"/> Change Visibility
-          </Button><br></br>
+              <Typography variant="h6" component="div">
+                &nbsp;&nbsp;Options
+              </Typography>
+              <Button
+                onClick={() => {
+                  handleDelete({
+                    variables: {
+                      fileId: content.fileId,
+                    },
+                  });
+                }}
+              >
+                <DeleteIcon color="action" /> Delete this File
+              </Button>
+              <br></br>
+              <Button
+                onClick={() => {
+                  handleChangeVisibility({
+                    variables: {
+                      fileId: content.fileId,
+                      show: !content.show,
+                    },
+                  });
+                }}
+              >
+                <RefreshIcon color="action" /> Change Visibility
+              </Button>
+              <br></br>
+            </Grid>
           </Grid>
-          </Grid>
-          </CardContent>
-        </Card>
-        )
-      };
-    return {
-        query,
-        setQuery,
-        CardForExam,
-        CardForFile,
-        documentType,
-        setDocumentType,
-        courseFilter,
-        setCourseFilter,
-        examFilter,
-        setExamFilter,
-        fileFilter,
-        setFileFilter,
-        queryData,
-        setQueryData
-    };
-}
+        </CardContent>
+      </Card>
+    );
+  };
+  return {
+    query,
+    setQuery,
+    CardForExam,
+    CardForFile,
+    documentType,
+    setDocumentType,
+    courseFilter,
+    setCourseFilter,
+    examFilter,
+    setExamFilter,
+    fileFilter,
+    setFileFilter,
+    queryData,
+    setQueryData,
+  };
+};
 
 export default useAdminChangeVisibility;
--- frontend/src/graphql/queryForAdmin.js
+++ frontend/src/graphql/queryForAdmin.js
@@ -1,144 +1,144 @@
-import {gql} from "@apollo/client";
+import { gql } from "@apollo/client";
 
 export const ADMIN_CHECK_SUBMISSION_QUERY = gql`
-    query{
-        courses(filter:{}){
-            year
-            semester
-            courseName
-            instructors
-            department
-            courseType
-            id
-            exams{
-                examName
-                examTime
-                id
-                files{
-                    questionDownloadLink
-                    questionViewLink
-                    answerDownloadLink
-                    answerViewLink
-                    remarks
-                    id
-                    pass
-                    examID
-                }
-            }
+  query {
+    courses(filter: {}) {
+      year
+      semester
+      courseName
+      instructors
+      department
+      courseType
+      id
+      exams {
+        examName
+        examTime
+        id
+        files {
+          questionDownloadLink
+          questionViewLink
+          answerDownloadLink
+          answerViewLink
+          remarks
+          id
+          pass
+          examID
         }
+      }
     }
-`
+  }
+`;
 
 export const ADMIN_COURSE_QUERY = gql`
-    query(
-        $year: Int
-        $Semester: String
-        $courseDept: String
-        $courseName: String
-        $courseType: String
-    ){
-        courses(
-            filter:{
-                year: $year
-                semester: $Semester
-                department: $courseDept
-                courseName: $courseName
-                courseType: $courseType
-            }
-        ){
-            id
-            year
-            semester
-            courseName
-            instructors
-            department
-            courseType
-            show
-            exams{
-                id
-            }
-        }
+  query (
+    $year: Int
+    $Semester: String
+    $courseDept: String
+    $courseName: String
+    $courseType: String
+  ) {
+    courses(
+      filter: {
+        year: $year
+        semester: $Semester
+        department: $courseDept
+        courseName: $courseName
+        courseType: $courseType
+      }
+    ) {
+      id
+      year
+      semester
+      courseName
+      instructors
+      department
+      courseType
+      show
+      exams {
+        id
+      }
     }
-`
+  }
+`;
 
 export const ADMIN_EXAM_QUERY = gql`
-query courses(
-        $year: Int
-        $Semester: String
-        $courseName: String
-        $courseDept: String
-        $courseType: String
-    ){
-        courses(
-            filter: {
-                year: $year
-                semester: $Semester
-                courseName: $courseName
-                department: $courseDept
-                courseType: $courseType
-            }
-        ){
-            id
-            year
-            semester
-            courseName
-            instructors
-            department
-            courseType
-            show
-            exams{
-                id
-                examName
-                examTime
-                show
-                files{
-                    id
-                }
-            }
+  query courses(
+    $year: Int
+    $Semester: String
+    $courseName: String
+    $courseDept: String
+    $courseType: String
+  ) {
+    courses(
+      filter: {
+        year: $year
+        semester: $Semester
+        courseName: $courseName
+        department: $courseDept
+        courseType: $courseType
+      }
+    ) {
+      id
+      year
+      semester
+      courseName
+      instructors
+      department
+      courseType
+      show
+      exams {
+        id
+        examName
+        examTime
+        show
+        files {
+          id
         }
+      }
     }
-`
+  }
+`;
 
 export const ADMIN_FILE_QUERY = gql`
-query courses(
-        $year: Int
-        $Semester: String
-        $courseName: String
-        $courseDept: String
-        $courseType: String
-    ){
-        courses(
-            filter: {
-                year: $year
-                semester: $Semester
-                courseName: $courseName
-                department: $courseDept
-                courseType: $courseType
-            }
-        ){
-            id
-            year
-            semester
-            courseName
-            instructors
-            department
-            courseType
-            show
-            exams{
-                id
-                examName
-                examTime
-                files{
-                    id
-                    questionDownloadLink
-                    questionViewLink
-                    answerDownloadLink
-                    answerViewLink
-                    remarks
-                    show
-                    pass
-                }
-            }
+  query courses(
+    $year: Int
+    $Semester: String
+    $courseName: String
+    $courseDept: String
+    $courseType: String
+  ) {
+    courses(
+      filter: {
+        year: $year
+        semester: $Semester
+        courseName: $courseName
+        department: $courseDept
+        courseType: $courseType
+      }
+    ) {
+      id
+      year
+      semester
+      courseName
+      instructors
+      department
+      courseType
+      show
+      exams {
+        id
+        examName
+        examTime
+        files {
+          id
+          questionDownloadLink
+          questionViewLink
+          answerDownloadLink
+          answerViewLink
+          remarks
+          show
+          pass
         }
+      }
     }
-`
+  }
+`;
